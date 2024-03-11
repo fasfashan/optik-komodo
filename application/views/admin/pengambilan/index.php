363ad2e432419ea952ca62bba113bd418b37021f
fasfashann <saintgloria.jkt@gmail.com>
2024-03-11 22:43:06 +0700

    fix breadcumb link on pengambilan page

--- application/views/admin/pengambilan/index.php
+++ application/views/admin/pengambilan/index.php
@@ -9,7 +9,7 @@
     <div class="container mt-5">
       <nav aria-label="breadcrumb">
         <ol class="breadcrumb">
-          <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+         <li class="breadcrumb-item"><a href="<?php echo base_url().'welcome'?>">Home</a></li>
           <li class="breadcrumb-item active" aria-current="page">
             Pengambilan kacamata
           </li>
